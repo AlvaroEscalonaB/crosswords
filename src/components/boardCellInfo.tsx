7ac3d66054057865a7f036563fe04785003434e6
alvaro14s <escalona.alvaro.13@gmail.com>
2024-12-25 21:58:48 -0300

    Add more styles for tooltip info

--- src/components/boardCellInfo.tsx
+++ src/components/boardCellInfo.tsx
@@ -1,5 +1,6 @@
 import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "@/components/ui/tooltip"
 import type { CellInformation } from "@custom_types/boardTypes"
+import { directions } from "src/translations/directions"
 
 interface CellProps {
   information: CellInformation[]
@@ -9,12 +10,15 @@ export default function BoardCellInfo({ information }: CellProps) {
   return (
     <TooltipProvider>
       <Tooltip delayDuration={50}>
-        <TooltipTrigger className="aspect-square size-10 border border-gray-300 bg-cyan-700 text-black flex justify-center items-center"></TooltipTrigger>
-        <TooltipContent className="bg-gray-400 border-gray-400" side="bottom">
+        <TooltipTrigger
+          className="aspect-square size-10 border border-gray-300 bg-cyan-700 text-black flex justify-center items-center"
+          tabIndex={-1}
+        ></TooltipTrigger>
+        <TooltipContent className="bg-gray-400 border-gray-400 flex flex-col gap-2" side="bottom">
           {information.map((dataDirection, index) => (
             <div key={index}>
-              <span className="font-bold mr-1">{dataDirection.direction}: </span>
-              <span className="font-medium">{dataDirection.text}</span>
+              <div className="block font-bold mr-1">{directions[dataDirection.direction]}</div>
+              <div className="font-medium">{dataDirection.text}</div>
             </div>
           ))}
         </TooltipContent>
